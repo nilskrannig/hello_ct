a0db4c1f68233d89213d6f6478f4b0db1ce6ed15
Nilsferd <nilskrannig@gmail.com>
2020-04-26 11:04:13 +0200

    projektstart: use const feedLimit

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
+const feedLimit = 5
+
 func main() {
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://www.heise.de/rss/heise-atom.xml")
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < feedLimit; i++ {
 		fmt.Println(feed.Items[i].Title)
 		fmt.Println(feed.Items[i].Link + "\n")
 	}
